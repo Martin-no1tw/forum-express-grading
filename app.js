1dd334dc0517d9bfeb171331ed0054b406bc6cb0
Martin-no1tw <kljhger7728@gmail.com>
2021-11-16 22:15:05 +0800

    feat:Update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const session = require('express-session')
 const passport = require('./config/passport')
 
 // 設定 view engine 使用 handlebars
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
@@ -22,6 +25,8 @@ app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
 app.use('/upload', express.static(__dirname + '/upload'))
+
+
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
