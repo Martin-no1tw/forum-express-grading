de750c79c93ef01f3bc8b3c422df35aec5ba5c9f
Martin-no1tw <kljhger7728@gmail.com>
2021-11-22 13:40:58 +0800

    feat:refactor router

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
 
-require('./routes')(app, passport)
+require('./routes')(app)
 
 module.exports = app
