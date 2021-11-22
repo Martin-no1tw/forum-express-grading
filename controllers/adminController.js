6f5a52571f1bd064ca3d580c113455c424888057
Martin-no1tw <kljhger7728@gmail.com>
2021-11-22 15:16:00 +0800

    feat:create api for admin one restaurant data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -14,6 +14,11 @@ const adminController = {
       return res.render('admin/restaurants', data)
     })
   },
+  getRestaurant: (req, res) => {
+    adminService.getRestaurant(req, res, (data) => {
+      return res.render('admin/restaurant', data)
+    })
+  },
   createRestaurant: (req, res) => {
     Category.findAll({
       raw: true,
@@ -62,15 +67,7 @@ const adminController = {
       })
     }
   },
-  getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: [Category]
-    }).then(restaurant => {
-      return res.render('admin/restaurant', {
-        restaurant: restaurant.toJSON()
-      })
-    })
-  },
+
   editRestaurant: (req, res) => {
     Category.findAll({
       raw: true,
